710c449ab493e138060f46d0109f1b031e432c45
Avneet <avneet_cochhar@hotmail.com>
2016-02-24 16:57:55 -0500

    ADDED coonfiguration and server viles

---
+++ SamplTest/config/env/development.js
@@ -0,0 +1,7 @@
+﻿//Development configuration options
+// 1 - database name
+// 2 - a secret string to sign the session identifier
+module.exports = {
+    db: 'mongodb://localhost/sample-db',
+    sessionSecret: 'myDevelopmentSecret'
+};
